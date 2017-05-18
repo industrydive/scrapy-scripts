d7f0c95b003ef4b9cea7d8bc008dd72c43018a3d
Bethany Morin <bmorin@industrydive.com>
2017-05-18 14:48:38 -0400

    Spider and processing of HR Tech 2017 exhibitors list created to get exhibitor name and website address

--- scraper/scraper/spiders/__init__.py
+++ scraper/scraper/spiders/__init__.py
@@ -1,4 +1,32 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scraper.items import HrTech2017Exhibitor
+
+
+class HrTech2017(CrawlSpider):
+    ''' Spider for HR Technology Conference 2017
+        https://industrydive.atlassian.net/browse/TECH-1623
+    '''
+
+    name = "hrtech2017"
+    allowed_domains = ["s23.a2zinc.net"]
+    start_urls = ["http://s23.a2zinc.net/clients/lrp/hrtechnologyconference2017/Public/exhibitors.aspx?Index=All"]
+    rules = [
+        Rule(
+            # article page
+            # follow links on this page
+            LinkExtractor(
+                allow=(
+                    u'\/(clients/lrp/hrtechnologyconference2017/Public/eBooth.aspx\?)',
+                ),
+            ),
+            callback='parse_exhibitor_detail',
+            follow=False,
+        ),
+    ]
+
+    # handle an article page here
+    def parse_exhibitor_detail(self, response):
+        item = HrTech2017Exhibitor()
+        item.process(response)
+        return item
