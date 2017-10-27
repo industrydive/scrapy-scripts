de17aa699588418f1d70f9139370ac86d28e2a91
Bethany Morin <bmorin@industrydive.com>
2017-10-27 12:09:00 -0400

    TECH-2834: NRF 2018 spider added

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -11,3 +11,20 @@ import scrapy
 class TradeShowExhibitor(scrapy.Item):
     website_url = scrapy.Field()
     exhibitor_name = scrapy.Field()
+
+
+class NRFTradeShowExhibitor(scrapy.Item):
+    website_url = scrapy.Field()
+    exhibitor_name = scrapy.Field()
+    contact_first_name = scrapy.Field()
+    contact_last_name = scrapy.Field()
+    contact_address1 = scrapy.Field()
+    contact_address2 = scrapy.Field()
+    contact_city = scrapy.Field()
+    contact_state = scrapy.Field()
+    contact_country = scrapy.Field()
+    contact_zip = scrapy.Field()
+    contact_phone = scrapy.Field()
+    contact_email = scrapy.Field()
+    sponsored_item = scrapy.Field()
+    sponsor_level = scrapy.Field()
--- scraper/scraper/spiders/__init__.py
+++ scraper/scraper/spiders/__init__.py
@@ -1,6 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scraper.items import TradeShowExhibitor
+from scraper.items import TradeShowExhibitor, NRFTradeShowExhibitor
 from bs4 import BeautifulSoup
 
 
@@ -11,13 +11,13 @@ class TradeShowSpider(CrawlSpider):
         'ITEM_PIPELINES': {
             'scraper.pipelines.TradeShowCompanyAndWebsitePipeline': 1,
         },
-        # 'CLOSESPIDER_ITEMCOUNT': 5,  # for testing - comment out for IRL
+        # 'CLOSESPIDER_ITEMCOUNT': 3,  # for testing - comment out for IRL
         # 'CONCURRENT_REQUESTS': 1,  # for testing - comment out for IRL
     }
 
     def parse_item(self, response):
         ''' Parse exhibitor detail page
-            Override this as
+            Override this as needed
         '''
         item = TradeShowExhibitor()
         html = BeautifulSoup(response.body, "html.parser")
@@ -57,7 +57,7 @@ class DistribuTech2018(TradeShowSpider):
         https://industrydive.atlassian.net/browse/TECH-1623
     '''
 
-    name = "distributech2017"
+    name = "distributech2018"
     allowed_domains = ["events.pennwell.com"]
     start_urls = ["http://events.pennwell.com/DTECH2018/Public/exhibitors.aspx?_ga=2.91461086.575732828.1507662078-248451487.1507662078"]
 
@@ -69,6 +69,73 @@ class DistribuTech2018(TradeShowSpider):
                     # ex: http://events.pennwell.com/DTECH2018/Public/eBooth.aspx
                     # ?IndexInList=0&FromPage=Exhibitors.aspx&ParentBoothID=&ListByBooth=true&BoothID=539921
                     u'\/(DTECH2018/Public/eBooth.aspx\?)',
+                ),
+            ),
+            callback='parse_item',
+            follow=False,  # no further info needed, stop at this page
+        ),
+    ]
+
+
+class nrf2018(TradeShowSpider):
+    ''' Spider for HR Technology Conference 2017
+        https://industrydive.atlassian.net/browse/TECH-1623
+    '''
+
+    name = "nrf2018"
+    allowed_domains = ["nrfbigshow.nrf.com"]
+    start_urls = ["https://nrfbigshow.nrf.com/exhibitors"]
+
+    def parse_item(self, response):
+        ''' Parse exhibitor detail page
+            Override this as needed
+        '''
+        import urllib2
+
+        item = NRFTradeShowExhibitor()
+        html = BeautifulSoup(response.body, "html.parser")
+
+        booth_obj = html.find('div', {'class': 'company_booths'})
+        booth_number = booth_obj.find('a').text
+
+        item['exhibitor_name'] = html.find('div', {'class': 'company_name'}).text
+
+        exhibitor_detail = BeautifulSoup(urllib2.urlopen("https://nrfbigshow.nrf.com/%s" % booth_number).read(), "html.parser")
+
+        website_class = exhibitor_detail.body.find('div', {'class': 'field--name-field-booth-website'})
+
+        link = website_class.find('a')
+        if link:
+            item['website_url'] = link.get('href')
+
+        generic_fields = [
+            'contact_first_name',
+            'contact_last_name',
+            'contact_address1',
+            'contact_address2',
+            'contact_city',
+            'contact_state',
+            'contact_country',
+            'contact_zip',
+            'contact_phone',
+            'contact_email',
+            'sponsored_item',
+            'sponsor_level',
+        ]
+
+        for field in generic_fields:
+            field_class = exhibitor_detail.body.find('div', {'class': 'field--name-field-booth-%s' % field.replace('_', '-')})
+            item[field] = field_class.find('div', {'class': 'field__item even'}).text
+
+        yield item
+
+    rules = [
+        Rule(
+            # rule for exhibitor detail link
+            LinkExtractor(
+                allow=(
+                    # ex: https://nrfbigshow.nrf.com/company/42-technologies
+                    u'\/company\/',
                 ),
             ),
             callback='parse_item',
